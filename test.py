5ec61eda38dbad91bec50b9ad68d40b4a96168e7
jesusrodriguezz <130525663+jesusrodriguezz@users.noreply.github.com>
2023-06-27 12:16:06 -0700

    Updated the test file with code to test GIFS

--- test.py
+++ test.py
@@ -1,3 +1,22 @@
-#Jesus Rodriguez
-print("Hello world")
+#Created by Jesus Rodriguez
+import requests
+import json
+
+
+API_KEY = 'K6YniUbHKX81UPXHjkdbz5YTWgeCX7ab'
+url = 'https://api.giphy.com/v1/gifs/trending'
+params = {
+    'api_key': API_KEY,
+    'limit': 1
+}
+response = requests.get(url, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+    gif_urls = [gif['images']['original']['url'] for gif in data['data']]
+
+    print(gif_urls)
+else:
+    print("Error occurred:", response.status_code)
+
 
