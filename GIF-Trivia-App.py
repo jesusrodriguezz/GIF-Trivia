bc30ad59472084ec1b977c347a19490a9297f00e
jesusrodriguezz <130525663+jesusrodriguezz@users.noreply.github.com>
2023-06-27 15:34:40 -0700

    Added a simple demo
    I added a simple demo of a user selecting a category and then generating a GIF from that category using the api

--- GIF-Trivia-App.py
+++ GIF-Trivia-App.py
@@ -1,15 +1,37 @@
-#Created by Jesus Rodriguez
+# Created by Jesus Rodriguez
+import requests
 def init():
     print("Enter name")
     name = input()
     print("Hello:", name, "Welcome to GIF-Trivia!")
+    
+    new_game(name)
     return ''
 
 def new_game(player):
+    print("Let's play the game.")
+    print("Which GIF category would you like to start with?")
+    print("Categories: movies")
+    category = input()
+    if category == "movies":
+        print("this where the api will get GIFS from category movies")
+        display_gifs()
+        pass
     return ''
 
 def keep_scores(player_input):
     return ''
 
 def display_gifs():
+    API_KEY = 'K6YniUbHKX81UPXHjkdbz5YTWgeCX7ab'
+    category_key = 'movies'
+    url = f"https://api.giphy.com/v1/gifs/random?api_key={API_KEY}&tag={category_key}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        gif_url = data["data"]["images"]["fixed_height_downsampled"]["url"]
+        print("GIF URL:", gif_url)
+    else:
+        print("Error:", response.status_code)
     return ''
+print(init())
